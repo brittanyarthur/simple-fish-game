e82f0caf30301f0a1efb14ecbbd8301513b87924
Brittany Arthur <bharthur@ucsc.edu>
2016-01-24 10:29:02 -0800

    added background (underwater background)

--- game.py
+++ game.py
@@ -5,9 +5,10 @@ from pygame.locals import *
 # 2 - Initialize the game
 pygame.init()
 width, height = 640, 480
-screen=pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((width, height))
  
 # 3 - Load images
+background = pygame.image.load("resources/images/underwater.jpg")
 player = pygame.image.load("resources/images/dude.png")
  
 # 4 - keep looping through
@@ -15,6 +16,7 @@ while 1:
     # 5 - clear the screen before drawing it again
     screen.fill(0)
     # 6 - draw the screen elements
+    screen.blit(background, (0, 0))
     screen.blit(player, (100,100))
     # 7 - update the screen
     pygame.display.flip()
